e4043735da393a7e358fd4e71f9d5e7b994b6d31
Neel Adwani <nprosofficial@gmail.com>
2021-09-26 11:16:22 +0000

    configured redirects and sessions

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from cryptography.fernet import Fernet
 from replit import db
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for
+from flask_session import Session
 
 app = Flask(__name__,template_folder='templates', static_folder='static')
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+db['testuser'] = 'pass123'
 
 def fetch_new_key():
   key = Fernet.generate_key()
@@ -36,8 +42,9 @@ def hello_world_post():
   password = request.form['password']
   print(username, password)
   if db[username] == password:
+    session['name'] = username
     print('you\'re in')
-  return render_template('login.html')
+    return redirect(url_for('prescrip'))
 
 @app.route('/doc')
 def doc():
@@ -61,7 +68,9 @@ def signup():
 
 @app.route('/history_and_prescription')
 def prescrip():
-  return 'history and prescription'
+  name = session['name']
+  print(name)
+  return render_template('patient.html')
 
 key = os.environ['key']
 f = Fernet(key)
